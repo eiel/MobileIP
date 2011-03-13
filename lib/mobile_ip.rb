890a2d12b7cf9f889ff31c7a8bfb390bf6b74299
Tomohiko Himura <eiel.hal@gmail.com>
2011-03-14 02:50:46 +0900

    共通処理を baseメソッド にまとめた

--- lib/mobile_ip.rb
+++ lib/mobile_ip.rb
@@ -1,78 +1,68 @@
-#require 'rubygems'
-
+require 'rubygems'
 require 'hpricot'
 require 'open-uri'
 require 'netaddr'
 
 class MobileIP
   def softbank
-    doc = Hpricot(open("http://creation.mb.softbank.jp/web/web_ip.html"))
-    records = []
-    (doc/"tr").each do |tr|
-      (tr/"td[@bgcolor=\"#eeeeee\"]").each do |td|
-        # rec << td.inner_html.strip
-        records << td.inner_html.sub(/&nbsp;&nbsp;/,'') 
+    base("http://creation.mb.softbank.jp/web/web_ip.html") do |doc|
+      records = []
+      (doc/"tr").each do |tr|
+        (tr/"td[@bgcolor=\"#eeeeee\"]").each do |td|
+          # rec << td.inner_html.strip
+          records << td.inner_html.sub(/&nbsp;&nbsp;/,'')
+        end
       end
+      records
     end
-    ret = []
-    records.uniq.each {|r|
-      a = NetAddr::CIDR.create(r)
-      a.enumerate.each {|rr|
-        ret << rr if (rr != a.ip && rr != a.last)
-      }
-    }
-    ret
   end
 
   def docomo
-    doc = Hpricot(open("http://www.nttdocomo.co.jp/service/imode/make/content/ip/"))
-    records = []
-    (doc/"ul[@class=\"normal txt\"]").each do |ul|
-      (ul/"li").each do |li|
-        # rec << td.inner_html.strip
-        # pp li.inner_html.to_s
-        /^\d+\.\d+\.\d+\.\d+\/\d+/ =~ li.inner_html.to_s
-        records << $&
+    base("http://www.nttdocomo.co.jp/service/imode/make/content/ip/") do |doc|
+      records = []
+      (doc/"ul[@class=\"normal txt\"]").each do |ul|
+        (ul/"li").each do |li|
+          # rec << td.inner_html.strip
+          # pp li.inner_html.to_s
+          /^\d+\.\d+\.\d+\.\d+\/\d+/ =~ li.inner_html.to_s
+          records << $&
+        end
       end
+      records
     end
-    ret = []
-    #pp records.uniq
-    records.uniq.each {|r|
-      a = NetAddr::CIDR.create(r)
-      a.enumerate.each {|rr|
-        # pp rr
-        ret << rr if (rr != a.ip && rr != a.last)
-      }
-    }
-    ret
   end
 
   def au
-    doc = Hpricot(open("http://www.au.kddi.com/ezfactory/tec/spec/ezsava_ip.html"))
-    records = []
-    ip = ""
-    (doc/"tr[@bgcolor=\"#ffffff\"]").each do |ul|
-      (ul/"div.TableText").each do |li|
-        # rec << td.inner_html.strip
-        #pp li.inner_html.to_s
-        #ip = ""
-        if /^\d+\.\d+\.\d+\.\d+/ =~ li.inner_html.to_s
-          ip = $&
-          #pp ip
+    base("http://www.au.kddi.com/ezfactory/tec/spec/ezsava_ip.html") do |doc|
+      records = []
+      ip = ""
+      (doc/"tr[@bgcolor=\"#ffffff\"]").each do |ul|
+        (ul/"div.TableText").each do |li|
+          # rec << td.inner_html.strip
+          #pp li.inner_html.to_s
+          #ip = ""
+          if /^\d+\.\d+\.\d+\.\d+/ =~ li.inner_html.to_s
+            ip = $&
+            #pp ip
+          end
+          if /^\/\d+/ =~ li.inner_html.to_s
+            ip = ip + $&
+            #pp ip
+          end
+          if /^\d+\.\d+\.\d+\.\d+\/\d+/ =~ ip.to_s
+            records << $&
+            ip = ""
+          end
         end
-        if /^\/\d+/ =~ li.inner_html.to_s
-          ip = ip + $&
-          #pp ip
-        end
-        if /^\d+\.\d+\.\d+\.\d+\/\d+/ =~ ip.to_s
-          records << $&
-          ip = ""
-        end 
       end
+      records
     end
+  end
 
+  def base(url,&block)
+    doc = Hpricot(open(url))
+    records = block[doc]
     ret = []
-    #pp records.uniq
     records.uniq.each {|r|
       a = NetAddr::CIDR.create(r)
       a.enumerate.each {|rr|
